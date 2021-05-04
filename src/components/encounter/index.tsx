ddba9901a6ef06eb1156889963093fe65823d380
Izzy George <george@work.co>
2021-05-04 11:26:07 -0700

    add draggable

--- src/components/encounter/index.tsx
+++ src/components/encounter/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 import { useRecoilValue } from 'recoil';
 import { campaignState } from '../../state/campaign';
 
@@ -8,9 +9,11 @@ export default function Encounter(): JSX.Element {
     <>
       {campaign.players.map((player) => {
         return (
-          <div key={player} className="w-max flex flex-col items-center">
-            <div className="h-6 w-6 bg-black" />{player}
-          </div>
+          <Draggable key={player}>
+            <div className="w-max flex flex-col items-center">
+              <div className="h-6 w-6 bg-black" />{player}
+            </div>
+          </Draggable>
         );
       })}
     </>
